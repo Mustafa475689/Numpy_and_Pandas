fc94cd06491ce912cd1d88a52271c09c548959b0
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-08 18:14:27 +0500

    Update array splitting example in main3.py
    Update array splitting example in main3.py

--- main3.py
+++ main3.py
@@ -56,6 +56,10 @@ import numpy as np
 # print(newarr[2])
 
 # Split 2d array..
-arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+# arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+# newarr = np.array_split(arr, 3)
+# print(newarr)
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
 newarr = np.array_split(arr, 3)
 print(newarr)
