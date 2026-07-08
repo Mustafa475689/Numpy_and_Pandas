75329109db89a1edccaddf23ff975fb9abaf2de2
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-08 13:01:03 +0500

    Uncomment code and update array dimensions
    Uncomment code and update array dimensions

--- main.py
+++ main.py
@@ -20,5 +20,13 @@ import numpy as np
 # print(td)
 
 # 3d array
-trd = np.array([[1,3,5,7],[2,4,6,8],[1,2,3,4]])
-print(trd)
+# trd = np.array([[[1,3,5,7],[2,4,6,8]],[[1,2,3,4],[5,6,7,8]]])
+# # use ndim to know the dimension of array
+# print(trd)
+# print(trd.ndim)
+
+
+arr = np.array([1, 2, 3, 4], ndmin=5)
+
+print(arr)
+print('number of dimensions :', arr.ndim)
