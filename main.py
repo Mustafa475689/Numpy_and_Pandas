6bdc50ae38281dee1adfc64c38d02243c631a3c7
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-08 12:54:26 +0500

    numpy
    numpy

---
+++ main.py
@@ -0,0 +1,24 @@
+# import pandas
+import numpy as np
+# mydataset = {
+#   'cars': ["BMW", "Volvo", "Ford"],
+#   'passings': [3, 7, 2]
+# }
+
+
+
+# arr = np.array([1, 2, 3, 4, 5])
+
+# print(arr)
+# print(type(arr))
+
+# tuple = np.array((2,4,6,8))
+# print(tuple)
+
+# 2d array
+# td = np.array([[1,3,5,7],[2,4,6,8]])
+# print(td)
+
+# 3d array
+trd = np.array([[1,3,5,7],[2,4,6,8],[1,2,3,4]])
+print(trd)
