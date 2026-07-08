c57182798cbaf486241d821e1d2030eace2e2ead
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-08 18:12:29 +0500

    Implement splitting of 2D array into sub-arrays
    Added functionality to split a 2D array into 3 parts.

--- main3.py
+++ main3.py
@@ -49,8 +49,13 @@ import numpy as np
 # newarre = np.array_split(arr, 4)
 # print(newarre)
 
-arr = np.array([1, 2, 3, 4, 5, 6])
+# arr = np.array([1, 2, 3, 4, 5, 6])
+# newarr = np.array_split(arr, 3)
+# print(newarr[0])
+# print(newarr[1])
+# print(newarr[2])
+
+# Split 2d array..
+arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
 newarr = np.array_split(arr, 3)
-print(newarr[0])
-print(newarr[1])
-print(newarr[2])
+print(newarr)
