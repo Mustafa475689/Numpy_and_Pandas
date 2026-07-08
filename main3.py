b1b89c6826a991c829f796f4e9654dce1ff911dc
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-08 17:54:52 +0500

    Implement array joining using np.stack
    Added example of joining arrays using stack function.

--- main3.py
+++ main3.py
@@ -8,3 +8,15 @@ import numpy as np
 # arr2 = np.array([4, 5, 6])
 # arr = np.concatenate((arr1, arr2)) # we use concatenate to join two array
 # print(arr)
+
+# 2d array join
+# arr1 = np.array([[1, 2], [3, 4]])
+# arr2 = np.array([[5, 6], [7, 8]])
+# arr = np.concatenate((arr1, arr2), axis=1)
+# print(arr)
+
+# Joining Arrays Using Stack Functions
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.stack((arr1, arr2), axis=1)
+print(arr)
