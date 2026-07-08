3a71ffabfdea597f3208a555cdfe6116dede74bd
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-08 17:37:09 +0500

    numpy
    numpy

---
+++ main3.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# ..........................
+# Array Join 
+# ...........................
+# join two array
+# arr1 = np.array([1, 2, 3])
+# arr2 = np.array([4, 5, 6])
+# arr = np.concatenate((arr1, arr2)) # we use concatenate to join two array
+# print(arr)
