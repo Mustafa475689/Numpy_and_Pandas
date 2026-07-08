b930305846b1c2aa52937fc61e5c35d582c33dd3
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-08 14:19:21 +0500

    Uncomment numpy array operations and examples
    Uncomment numpy array operations and examples

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ import numpy as np
 
 
 # Slicing
-arr = np.array([1, 2, 3, 4, 5, 6, 7])
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
 # print(arr[1:5])
 # print(arr[2:4])
 # print(arr[5:6])
@@ -62,4 +62,11 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7])
 # step
 # print(arr[1:5:2])
 # print(arr[1:5:1])
-print(arr[::2])
+# print(arr[::2])
+
+# 2d slicing
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+print(arr[1, 1:4])
+print(arr[0:2, 2]) # from both element return index 2
+print(arr[0:2, 1:4]) #this will return the 2d array
