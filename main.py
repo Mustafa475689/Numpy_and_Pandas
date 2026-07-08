f7bdaf9de3c89e64c0fa5e885f61a01a5e385dd3
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-08 13:46:59 +0500

    Uncomment numpy array operations and slicing examples
    Uncomment numpy array operations and slicing examples

--- main.py
+++ main.py
@@ -51,8 +51,15 @@ import numpy as np
 
 # Slicing
 arr = np.array([1, 2, 3, 4, 5, 6, 7])
-print(arr[1:5])
-print(arr[2:4])
-print(arr[5:6])
-print(arr[4:])
-print(arr[:4])
+# print(arr[1:5])
+# print(arr[2:4])
+# print(arr[5:6])
+# print(arr[4:])
+# print(arr[:4])
+# Negetive slicing
+# print(arr[-5: -4])
+# print(arr[:-4])
+# step
+# print(arr[1:5:2])
+# print(arr[1:5:1])
+print(arr[::2])
