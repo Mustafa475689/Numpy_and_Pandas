bef101a8bd74c7f6e854726c017f095101b0e718
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-09 13:07:58 +0500

    Enhance random choice functionality with 2D array
    Added functionality to generate a 2D random array based on defined probabilities.

--- Numpy/Numpy Random/main.py
+++ Numpy/Numpy Random/main.py
@@ -37,3 +37,7 @@ from numpy import random
 # We can generate random numbers based on defined probabilities using the choice() method of the random module.
 x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(100))
 print(x)
+
+# 2d random probability ...........
+x = random.choice([1, 2, 4, 6], p=[0.1, 0.3, 0.6, 0.0], size=(3, 5))
+print(x)
