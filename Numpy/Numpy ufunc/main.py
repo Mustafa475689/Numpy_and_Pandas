7c0c6c000ce24388d87e05a3c21c170ae1d23462
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-09 14:59:36 +0500

    Add template for custom numpy ufunc creation
    Added a template for creating custom ufuncs with numpy.

---
+++ Numpy/Numpy ufunc/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Create Your Own ufunc
+# def myadd(x, y):
+#   return x+y
+# myadd = np.frompyfunc(myadd, 2, 1)
+# print(myadd([1, 2, 3, 4], [5, 6, 7, 8]))
+
+# print(type(np.add))
+
