d20d5fc337471ad63f85d36d5270a18aeb6e980e
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-09 15:47:07 +0500

    Uncomment code examples for NumPy functions
    Uncomment code examples for NumPy functions

--- Numpy/Numpy ufunc/main.py
+++ Numpy/Numpy ufunc/main.py
@@ -105,10 +105,17 @@ import numpy as np
 # print("Addition ", newarr)
 # print("Summation ", summation)
 
-# Summation Over an Axis
-arr1 = np.array([1, 2, 3])
-arr2 = np.array([1, 2, 3])
+# # Summation Over an Axis ...........
+# arr1 = np.array([1, 2, 3])
+# arr2 = np.array([1, 2, 3])
 
-newarr = np.sum([arr1, arr2], axis=1)
+# newarr = np.sum([arr1, arr2], axis=1)
+# print(newarr)
+
+# Cummulative Sum ............ Cummulative sum means partially adding the elements in array.
+# E.g. The partial sum of [1, 2, 3, 4] would be [1, 1+2, 1+2+3, 1+2+3+4] = [1, 3, 6, 10].
+# Perfom partial sum with the cumsum() function.
+arr = np.array([1, 2, 3])
+newarr = np.cumsum(arr)
 
 print(newarr)
