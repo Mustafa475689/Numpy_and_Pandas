de9fcd8fa28b1ee6bfa5c300b844a354a206cbb2
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-09 11:48:08 +0500

    Refactor filter logic to create filtered array
    Updated the filter logic to create a new filtered array based on elements greater than 5.

--- filter.py
+++ filter.py
@@ -2,7 +2,23 @@
 # ..........................
 import numpy as np
 
-arr = np.array([41, 42, 43, 44])
-x = [True, False, True, False]
-newarr = arr[x]
+# arr = np.array([41, 42, 43, 44])
+# x = [True, False, True, False]
+# newarr = arr[x]
+# print(newarr)
+
+# .... Creating the Filter Array ............
+arr = np.array([1, 3, 4 ,6, 8])
+filter_arr = []
+
+for element in arr:
+    if element > 5:
+        filter_arr.append(True)
+    else:
+        filter_arr.append(False)
+
+newarr = arr[filter_arr]
+
+print(filter_arr)
 print(newarr)
+
