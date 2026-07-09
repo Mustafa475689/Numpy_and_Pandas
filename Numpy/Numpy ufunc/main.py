5aea91662847ce7618d3522a5f99890c0867f7cf
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-09 15:02:33 +0500

    Implement simple arithmetic with numpy arrays
    Added simple arithmetic operations using numpy arrays.

--- Numpy/Numpy ufunc/main.py
+++ Numpy/Numpy ufunc/main.py
@@ -8,3 +8,17 @@ import numpy as np
 
 # print(type(np.add))
 
+# ...... Check if add() function is ufunc or not
+# if type(np.add) == np.ufunc:
+#   print('add is ufunc')
+# else:
+#   print('add is not ufunc')
+
+# Simple arithmetic
+arr1 = np.array([10, 11, 12, 13, 14, 15])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+newarr = np.add(arr1, arr2)
+sub = np.subtract(arr1, arr2)
+print(newarr)
+print(sub)
