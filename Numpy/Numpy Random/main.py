9ec5edf72106dfb3be6d4b344b3109d9d71c57ae
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-09 13:14:04 +0500

    Implement random array shuffling and permutation examples
    Added examples for shuffling and permuting arrays using numpy.

--- Numpy/Numpy Random/main.py
+++ Numpy/Numpy Random/main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy import random
 # Random # 
 # .........................................#
@@ -35,9 +36,21 @@ from numpy import random
 #       Random Distribution
 # ................................ #
 # We can generate random numbers based on defined probabilities using the choice() method of the random module.
-x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(100))
-print(x)
+# x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(100))
+# print(x)
 
-# 2d random probability ...........
-x = random.choice([1, 2, 4, 6], p=[0.1, 0.3, 0.6, 0.0], size=(3, 5))
-print(x)
+# # 2d random probability ...........
+# x = random.choice([1, 2, 4, 6], p=[0.1, 0.3, 0.6, 0.0], size=(3, 5))
+# print(x)
+
+# ................................ #
+#       Random Permutation
+# ................................ #
+# Shuffling Arrays
+arr = np.array([2, 4, 6, 8])
+random.shuffle(arr)
+print(arr)
+
+# Generating Permutation of Arrays
+arr = np.array([1, 2, 3, 4, 5])
+print(random.permutation(arr))
