a66d7d5adc01fe24587659c67572250909163578
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-09 12:45:58 +0500

    Enhance random number generation examples
    Updated random number generation examples to include random floats and arrays.

--- Numpy/Numpy Random/main.py
+++ Numpy/Numpy Random/main.py
@@ -2,5 +2,22 @@ from numpy import random
 # Random # 
 # .........................................#
 # Generate Randomnumbers ..................# Generate a random integer from 0 to 100:
-x = random.randint(100)
+# x = random.randint(100)
+# print(x)
+
+# Generate RAndom float ....................
+# x = random.rand()
+# print(x)
+# The rand() method also allows you to specify the shape of the array.
+x = random.rand(5)
+print(x)
+# 2d random float array
+x = random.rand(3, 5)   
+print(x)
+
+# Generate Random Array................ The randint() method takes a size parameter where you can specify the shape of an array.
+# x=random.randint(100, size=(5))
+# print(x)
+# 2d random array
+x = random.randint(100, size=(3, 5))
 print(x)
