87f1595f89de6cf082cd882822c92e4fde0cb360
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-09 15:35:22 +0500

    Implement custom logarithm function with frompyfunc
    Added a custom logarithm function using numpy's frompyfunc.

--- Numpy/Numpy ufunc/main.py
+++ Numpy/Numpy ufunc/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import log
 
 # Create Your Own ufunc
 # def myadd(x, y):
@@ -75,10 +76,18 @@ import numpy as np
 # ............................... #
 # Numpy Logs
 # ............................... #
-# Log at Base 2
-arr = np.arange(1, 10)
-print(np.log2(arr))
+# # Log at Base 2
+# arr = np.arange(1, 10)
+# print(np.log2(arr))
 
-# Log at Base 10
-arr = np.arange(1, 10)
-print(np.log10(arr))
+# # Log at Base 10
+# arr = np.arange(1, 10)
+# print(np.log10(arr))
+
+# # Natural Log, or Log at Base e
+# arr = np.arange(1, 10)
+# print(np.log(arr))
+
+# Log at Any Base
+nplog = np.frompyfunc(log, 2, 1)
+print(nplog(100, 15))
