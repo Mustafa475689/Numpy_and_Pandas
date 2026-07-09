6dbf7388409a02dd4554641918fee239dc42bffc
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-09 16:02:04 +0500

    Add computation of discrete difference twice
    Compute the discrete difference of an array twice using np.diff.

--- Numpy/Numpy ufunc/main2.py
+++ Numpy/Numpy ufunc/main2.py
@@ -22,7 +22,22 @@ import numpy as np
 # print(newarr)
 
 # Cummulative Product
-arr = np.array([5, 6, 7, 8])
+# arr = np.array([5, 6, 7, 8])
 
-newarr = np.cumprod(arr)
+# newarr = np.cumprod(arr)
+# print(newarr)
+
+# ......................... #
+# NumPy Differences
+# ......................... #
+# Differences ..... A discrete difference means subtracting two successive elements.
+# E.g. for [1, 2, 3, 4], the discrete difference would be [2-1, 3-2, 4-3] = [1, 1, 1]........ use the diff() function.
+
+# arr = np.array([10, 15, 25, 5])
+# newarr = np.diff(arr)
+# print(newarr)
+
+# Compute discrete difference of the following array twice: ..... We can perform this operation repeatedly by giving parameter n.
+arr = np.array([10, 15, 25, 5])
+newarr = np.diff(arr, n=2)
 print(newarr)
