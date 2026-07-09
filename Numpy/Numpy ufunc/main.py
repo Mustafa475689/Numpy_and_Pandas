fe065c07f2211be729fa5a26b4126f4f6a7099a2
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-09 15:18:39 +0500

    Uncommented floor function example in main.py
    Uncommented floor function example in main.py

--- Numpy/Numpy ufunc/main.py
+++ Numpy/Numpy ufunc/main.py
@@ -50,5 +50,19 @@ import numpy as np
 # ............................... #
 # Truncation
 # Remove the decimals, and return the float number closest to zero. Use the trunc() and fix() functions.
-arr = np.trunc([-3.1666, 3.6667])
+# arr = np.trunc([-3.1666, 3.6667])
+# print(arr)
+
+# # ...... fix() ........
+# arr = np.fix([-3.1666, 3.6667])
+# print(arr)
+
+# .... Rounding .....
+# The around() function increments preceding digit or decimal by 1 if >=5 else do nothing.
+# arr = np.around(3.1666, 2) 
+# print(arr)
+
+# ......... Floor ......
+# The floor() function rounds off decimal to nearest lower integer.
+arr = np.floor([-3.1666, 3.6667])
 print(arr)
