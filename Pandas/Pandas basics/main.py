99b9d77fdc6b86efb1dd914ccfba78f148fac93c
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-11 22:48:56 +0500

    Uncomment code and adjust max rows for display
    Uncomment code and adjust max rows for display

--- Pandas/Pandas basics/main.py
+++ Pandas/Pandas basics/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 #   'cars': ["BMW", "Volvo", "Ford"],
 #   'passings': [3, 7, 2]
 # }
-
+# ...........................................
 # myvar = pd.DataFrame(mydataset)
 # print(myvar)
 
